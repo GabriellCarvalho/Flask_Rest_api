f373340105a3fa4191faa67641f43296ff0770b5
Gabriel Lopes Carvalho <gabriellopescarvalho@live.com>
2021-11-12 15:00:11 -0300

    commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+desenvolvedores = [
+    {'nome':'Gabriel',
+     'habilidade':['python','Flask']
+     },
+    {'nome': 'Lopes',
+     'habilidade':['python', 'Django']}
+]
+
+
+@app.route('/dev/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status':'erro', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status':'erro', 'mensagem':mensagem} 
+        return jsonify(response)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status': 'sucesso', 'mensagem':'Registro exluido'})
+
+@app.route('/dev/', methods=['POST','GET'])
+def lista_desenvolvedores():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        desenvolvedores.append(dados)
+        return jsonify({'status':'sucesso', 'mensagem':'Registro inserido'})
+    elif request.method == 'GET':
+        return jsonify(desenvolvedores)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
